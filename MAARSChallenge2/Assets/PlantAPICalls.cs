df5e458cb0b8d2f4e15d89f48c8a2e765af70be6
Keyana Wright <43353330+KWright8@users.noreply.github.com>
2020-04-02 19:53:23 -0400

    successfully makes request

---
+++ MAARSChallenge2/Assets/PlantAPICalls.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.IO;
+using UnityEngine.Networking;
+
+public class PlantAPICalls : MonoBehaviour
+{
+    public void get()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("https://maars-api.herokuapp.com/"));
+
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+
+       // Request and wait for the desired page.
+       yield return webRequest.SendWebRequest();
+
+       if (webRequest.isNetworkError)
+       {
+                Debug.Log( "Error: " + webRequest.error);
+       } else {
+                Debug.Log("SUCCESS" + webRequest.downloadHandler.text);
+       }
+    }
+    }
+
+
+
---
+++ MAARSChallenge2/Assets/Scripts/GetStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System.Net;
+using System.IO;
+
+
+
+public class GetStatus : MonoBehaviour
+{
+    public String id;
+
+    public void getStatus(int ID)
+    {
+        // get the ID of the current image target which is the same as the plant ID to use as the key in database
+        // Make API call to get the date of last service for that plant ID
+        // Get the curent month and day
+        // compare current date and the date recived from api get the total number of days that have passed
+        // make funcion call or set up enum to display proper status
+
+
+        // get the local date and universal date
+        // DateTime localDate = DateTime.Now;
+        // DateTime utcDate = DateTime.UtcNow;
+
+        // Debug.Log("This is the date" + localDate);
+        // Debug.Log("This is the date utcDate" + utcDate);
+
+       
+
+    }
+
+    // get the id to get the QR code
+    public void setID(String ID)
+    {
+        id = ID;
+    }
+
+    private void displayStatus()
+    {
+        // nada right now.
+    }
+
+}
